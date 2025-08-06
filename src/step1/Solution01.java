904d21f356b90f2448534d8e07afb2d3edf58c96
notlikelion <notlikelion@gmail.com>
2025-08-06 10:18:19 +0900

    스레드

--- src/step1/Solution01.java
+++ src/step1/Solution01.java
@@ -5,14 +5,22 @@ import java.util.Random;
 public class Solution01 {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 3; i++) {
-            System.out.println("%d회차".formatted(i + 1));
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println("%d회차".formatted(i + 1));
+//            randomWork();
+//        }
+        Runnable task = () -> {
             randomWork();
+        };
+//        Runnable task = Solution01::randomWork;
+        for (int i = 0; i < 3; i++) {
+            new Thread(task, "Worker-%d".formatted(i + 1)).start();
         }
         System.out.println(System.currentTimeMillis() - startTime);
     }
 
     public static void randomWork() {
+        System.out.println("작업 시작");
         Random random = new Random();
         // 로직
         // 프로세스 - 스레드
@@ -23,6 +31,6 @@ public class Solution01 {
         } catch (Exception e) {
             throw new RuntimeException();
         }
-
+        System.out.println("작업 종료");
     }
 }
